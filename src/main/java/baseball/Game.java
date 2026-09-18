622851f108ffb9e05e120dad32772a440d419bcb
Jisu Hong <jaylovegood@snu.ac.kr>
2026-09-18 13:05:05 +0900

    [feature] 59p

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,8 +2,14 @@ package baseball;
 
 public class Game {
 
-    public void guess(String guessNumber) {
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
         verifyInput(guessNumber);
+        if (guessNumber.equals(question)){
+            return new GuessResult(true, 3, 0);
+        }
+        return null;
     }
 
     private static void verifyInput(String guessNumber) {
